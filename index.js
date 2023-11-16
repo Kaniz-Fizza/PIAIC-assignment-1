9c9fb79ddebbb67d7e0ede9ee262fd8e0b31051e
Kaniz-Fizza <150988080+Kaniz-Fizza@users.noreply.github.com>
2023-11-16 09:38:39 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+"use strict";
+///////////////````assignment 1````///////////////
+// Question no 1
+// Declare variables for a person's name, age, and whether they are a student, Print these variables to the console.
+var username = "haider";
+var age = 18;
+var student = true;
+console.log(username);
+console.log(age);
+console.log(student);
+// Question no 2
+// Declare two variables with numeric values, Perform addition, subtraction, multiplication, and division on these variables, Print the results to the console.
+var num1 = 1;
+var num2 = 2;
+var num3 = 3;
+var num4 = 4;
+var num5 = 5;
+var num6 = 6;
+var num7 = 7;
+var num8 = 8;
+var num9 = 9;
+var num10 = 10;
+console.log(num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10);
+console.log(num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10);
+console.log(num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10);
+console.log(num1 / num2 / num3 / num4 / num5 / num6 / num7 / num8 / num9 / num10);
+// Question no 3
+// Declare a string variable with your favorite programming language, Print the final sentence to the console.
+let favoriteLanguage = "TypeScript";
+console.log(`My favorite programming language is = ${favoriteLanguage}.`);
